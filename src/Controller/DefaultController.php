08f9dce898ad9db3f6dc350bea32badecdead1f6
Mohosin <mohosin16103308@gmail.com>
2019-06-15 21:00:45 +0600

    Controller 01

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -7,7 +7,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class DefaultController  extends AbstractController
 {
-     /**
+    /**
      * Matches /index exactly
      *
      * @Route("/index", name="index")
@@ -15,24 +15,41 @@ class DefaultController  extends AbstractController
     public function index()
     {
         $number = random_int(0, 100);
-        dump( $number);
-        dump( $number);
-
+        dump($number);
+        dump($number);
+        // redirect to a route with parameters
+        return $this->redirectToRoute('about', ['max' => 10]);
         return new Response(
-            '<html><body>Lucky number: '.$number.'</body></html>'
+            '<html><body>Lucky number: ' . $number . '</body></html>'
         );
     }
-    
-     /**
+
+    /**
      * Matches /about exactly
      *
-     * @Route("/about", name="about")
+     * @Route("/about/{max}", name="about")
      */
-    public function about()
+    public function about($max)
     {
-        
+        dump($max);
         return new Response(
             '<html><body>I am Md.Mohosin Miah student of IUBAT</body></html>'
         );
     }
+
+
+
+    /**
+     * Matches /country_name exactly
+     *
+     * @Route("/country_name/{country}", name="country_name")
+     */
+    public function country_name($country)
+    {
+        dump($country);
+        return new Response(
+            "<html><body>Hi,You from $country</body></html>"
+        );
+    }
+
 }

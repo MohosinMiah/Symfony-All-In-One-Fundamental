edc738ba3b7a4d84d07f5e0fce4d6e05476ffbe6
Mohosin <mohosin16103308@gmail.com>
2019-06-15 22:23:30 +0600

    Views 01

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -1,55 +1,27 @@
 <?php
+
 namespace App\Controller;
 
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
 
-class DefaultController  extends AbstractController
+class DefaultController extends AbstractController
 {
     /**
-     * Matches /index exactly
-     *
-     * @Route("/index", name="index")
+     * @Route("/default", name="default")
      */
     public function index()
     {
-        $number = random_int(0, 100);
-        dump($number);
-        dump($number);
-        // redirect to a route with parameters
-        return $this->redirectToRoute('about', ['max' => 10]);
-        return new Response(
-            '<html><body>Lucky number: ' . $number . '</body></html>'
-        );
+        $data =[
+'one',
+'two',
+'three'
+
+        ];
+        return $this->render('default/index.html.twig', [
+            'controller_name' => 'DefaultController',
+            'msg' =>"Send Data from controller to view ",
+            'datas' =>$data,
+        ]);
     }
-
-    /**
-     * Matches /about exactly
-     *
-     * @Route("/about/{max}", name="about")
-     */
-    public function about($max)
-    {
-        dump($max);
-        return new Response(
-            '<html><body>I am Md.Mohosin Miah student of IUBAT</body></html>'
-        );
-    }
-
-
-
-    /**
-     * Matches /country_name exactly
-     *
-     * @Route("/country_name/{country}", name="country_name")
-     */
-    public function country_name($country)
-    {
-        dump($country);
-        return new Response(
-            "<html><body>Hi,You from $country</body></html>"
-        );
-    }
-
 }

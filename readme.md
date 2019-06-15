e586decb135b08adda351e17a2135c5dde3a7313
Mohosin <mohosin16103308@gmail.com>
2019-06-15 20:39:49 +0600

    Routes 01

---
+++ readme.md
@@ -0,0 +1,1 @@
+#Symfony Fundamental Basic To Advanced
--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,15 +2,37 @@
 namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-class DefaultController
+class DefaultController  extends AbstractController
 {
+     /**
+     * Matches /index exactly
+     *
+     * @Route("/index", name="index")
+     */
     public function index()
     {
         $number = random_int(0, 100);
+        dump( $number);
+        dump( $number);
 
         return new Response(
             '<html><body>Lucky number: '.$number.'</body></html>'
+        );
+    }
+    
+     /**
+     * Matches /about exactly
+     *
+     * @Route("/about", name="about")
+     */
+    public function about()
+    {
+        
+        return new Response(
+            '<html><body>I am Md.Mohosin Miah student of IUBAT</body></html>'
         );
     }
 }
